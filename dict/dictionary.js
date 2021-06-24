45d438e2789d8f8159a57b7e402668607d01d2ee
Bayarbat Batbayar <b.bayarbat@gmail.com>
2021-06-24 15:58:02 -0500

    dict

--- dict/dictionary.js
+++ dict/dictionary.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var wordMod = require('./word');
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 var server = app.listen(8080,function(){
     console.log('Listening...');    
@@ -13,12 +14,12 @@ app.get('/', cors(), function(req, res, next) {
     res.json('Hello World');
 });
 
-app.get('/dictionary', function(req, res, next) {
+app.get('/dictionary', cors(), function(req, res, next) {
     var word = req.query.word;
     wordMod.search(req, res, word);
 });
 
-app.post('/dictionary', function(req, res, next) {
+app.post('/dictionary', cors(), function(req, res, next) {
     var word = req.body.word;
     wordMod.search(req, res, word);
 });
